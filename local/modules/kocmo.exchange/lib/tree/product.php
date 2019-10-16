132cd79e91b90d0e71ca8ed5e1f6691abb4fe61c
Alexey FromWork <latigovskiy@selvin.local>
2019-10-16 17:31:38 +0300

    1

--- local/modules/kocmo.exchange/lib/tree/product.php
+++ local/modules/kocmo.exchange/lib/tree/product.php
@@ -6,6 +6,10 @@ namespace Kocmo\Exchange\Tree;
 
 class Product extends Handler
 {
+
+    protected $conformity = [];
+    protected $conformityName = [];
+
     function __construct()
     {
         parent::__construct();
@@ -55,6 +59,64 @@ class Product extends Handler
 
         return count($arrForDb) ? $arrForDb : false;
     }
+
+    public function send3(){
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request('GET', 'http://kocmo1c.sellwin.by/Kosmo_Sergey/hs/Kocmo/GetCatalog');
+        $arrForDb = [];
+
+        if ($response->getStatusCode() == 200) {
+
+            $outArr = json_decode($response->getBody(), true);
+            //$firstIter = true;
+
+            foreach( $outArr as $key => $item ){
+
+                //if($firstIter) {
+                    $prepareItem = [];
+
+                    foreach ($item as $k => $v) {
+
+                        $arItem = $this->getConformity($k, $v);
+
+//                        if (!empty($arItem['VALUE']) && !isset($this->conformityName[$arItem['VALUE']]) ) {
+//                            $this->conformityName[$arItem['VALUE']] = $k;
+//                        }
+                    }
+                    //$firstIter = false;
+                //}
+
+                $arrForDb[$item['UID']]['JSON'] = json_encode($item);
+                $arrForDb[$item['UID']]["IMG_GUI"] = $item[ $this->conformity[$this->arParams['PIC_FILE']] ];
+                $outArr[$key] = null;
+            }
+        } else {
+            throw new \Error("error: status: " . $response->getStatusCode());
+        }
+    }
+
+    private function getConformity($k, $v){
+
+        if( isset($this->conformity[$k]) && !empty($this->conformity[$k][$v]) ) {
+            return $this->conformity[$k][$v];
+        }
+        else{
+            $client = new \GuzzleHttp\Client();
+            $response = $client->request('GET', '/' . $k);
+            if ($response->getStatusCode() == 200) {
+                $arOut = json_decode($response->getBody(), true);
+                if(count($arOut)){
+                    $this->conformity[$k] = $arOut;
+                    if( !empty($this->conformity[$k][$v]) ) {
+                        return $this->conformity[$k][$v];
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
 
     public function getProductParentsXmlId(){
 
